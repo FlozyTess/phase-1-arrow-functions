079126519faf52dd61911a534500634125005f10
FlozyTess <cecilrency23@gmail.com>
2024-12-04 15:15:22 +0300

    implemented arrow functions

---
+++ index.js
@@ -0,0 +1,10 @@
+// Has a function expression called divide.
+const divide = function(a, b) {
+    return a / b;}
+    console.log(divide(2000,100))
+//has an arrow function called square
+const square =(x) => x * x;
+console.log(square(5))
+//has an arrow function called add
+const add =(a,b) => a+b;
+console.log(add(5,2))
